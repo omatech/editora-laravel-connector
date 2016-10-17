7ffaed003719294ddc1948102a27d9c8058454f4
Christian Bohollo <cbohollo@omatech.com>
2016-10-17 12:04:53 +0200

    initial commit

---
+++ src/ConnectorServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Omatech\Connector;
+
+use Illuminate\Support\ServiceProvider;
+use Omatech\Editora\Utils\Editora;
+use Omatech\Editora\Extractor\Extractor;
+
+class ConnectorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/Routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->db = [
+            'dbname' => env('DB_DATABASE'),
+            'dbuser' => env('DB_USERNAME'),
+            'dbpass' => env('DB_PASSWORD'),
+            'dbhost' => env('DB_HOST'),
+        ];
+
+        $this->app->singleton('Extractor', function ($app) {
+            return new Extractor($this->db);
+        });
+
+        $this->app->singleton('Editora', function ($app) {
+            return new Editora($this->db);
+        });
+
+        $this->app->make('Omatech\Connector\EditoraController');
+    }
+}
---
+++ src/EditoraController.php
@@ -0,0 +1,112 @@
+<?php
+namespace Omatech\Connector;
+
+use App;
+Use Session;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Omatech\Editora\Utils\Editora as Utils;
+use Omatech\Editora\Extractor\Editora as Extractor;
+
+session_start(); //Editora
+
+class EditoraController extends Controller
+{
+    protected $utils;
+    protected $extractor;
+
+    public function __construct() {
+        $this->utils = App::make('Editora');
+        $this->extractor = App::make('Extractor');
+    }
+
+    public function __invoke(Request $request) {
+        /**
+         *
+         **/
+        $language = $request->route('language');
+        $niceUrl  = $request->route('nice_url');
+        $req_info = $request->input('req_info');
+
+        $preview = $this->editMode($req_info);
+
+        /**
+         *
+         **/
+        $this->setLanguageFromSession();
+        if($language) $this->setLanguageToSession($language);
+
+        /**
+         *
+         **/
+        $urlData = $this->utils->get_url_data($language, $niceUrl);
+
+        /**
+         *
+         **/
+        if($urlData['type'] === "Error") abort(404);
+        if($urlData['type'] === "ChangeLanguage") return redirect('/');
+
+        /**
+         *
+         **/
+        $className = 'App\\Http\\Controllers\\Editora\\'.$urlData['class_tag'];
+        $class = new $className;
+
+        $class->urlData = $urlData;
+        $class->language = $this->getCurrentLanguage();
+        $class->inst_id = (array_key_exists('id', $urlData)) ? $urlData['id'] : 1;
+        $class->preview = $preview;
+
+        return $class->render();
+    }
+
+    /**
+     *
+     **/
+    protected function extract($query = null, $params = null, $object, $ferret) {
+        return $this->extractor->extract($query, $params, $object, $ferret);
+    }
+
+    /**
+     *
+     **/
+    private function editMode($req_info) {
+        $preview = false;
+
+        if($req_info !== null && $req_info == 1) {
+            if(isset($_SESSION['user_id']) && $_SESSION['user_id'] != '') {
+                $preview = true;
+            } else {
+                die('Not connected to CMS!');
+            }
+        }
+
+        return $preview;
+    }
+
+    /**
+     *
+     **/
+    private function setLanguageFromSession() {
+        $lang = (Session::get('locale') !== null) ? Session::get('locale') : env('APP_LANG');
+        $_SESSION['u_lang'] = Session::get('locale');
+        App::setLocale($lang);
+    }
+
+    /**
+     *
+     **/
+    private function getCurrentLanguage() {
+        return App::getLocale();
+    }
+
+    /**
+     *
+     **/
+    private function setLanguageToSession($language) {
+        Session::put('locale', $language);
+        $this->setLanguageFromSession();
+    }
+}
